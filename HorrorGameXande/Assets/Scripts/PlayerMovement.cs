c29645d79ad498edc604bbb185a93ae4563ba852
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-08-02 08:49:05 -0300

    Movimentação Concluída

--- HorrorGameXande/Assets/Scripts/PlayerMovement.cs
+++ HorrorGameXande/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayerRelativeToCamera();
     }
+    
     void MovePlayerRelativeToCamera()
     {
         //Get Player Input
@@ -20,14 +24,18 @@ public class PlayerMovement : MonoBehaviour
         //Get Camera Normalized Directional Vectors
         Vector3 forward = Camera.main.transform.forward;
         Vector3 right = Camera.main.transform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward = forward.normalized;
+        right = right.normalized;
 
         //Create direction-relative-input vectors
-        Vector3 forwardRelativeVerticalInput = playerVerticalInput * forward;
-        Vector3 rightRelativeHorizontalInput = playerHorizontalInput * forward;
+        Vector3 forwardRelativeVerticalInput = (playerVerticalInput * forward) * speed/100;
+        Vector3 rightRelativeHorizontalInput = (playerHorizontalInput * forward) * speed/100;
 
         //Create and apply camera relative movement
-        Vector3 camerarelativeMovement = forwardRelativeVerticalInput + rightRelativeHorizontalInput;
-        this.transform.Translate(camerarelativeMovement, Space.World);
+        Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeHorizontalInput;
+        this.transform.Translate(cameraRelativeMovement, Space.World);
     }
 }
 //https://www.youtube.com/watch?v=7kGCrq1cJew
